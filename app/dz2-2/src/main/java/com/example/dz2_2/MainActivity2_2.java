d0e2bd80bd0b3dd9d60a071eb566ce15b9b60e5c
vasilio13 <vasilkhaliaukin@gmail.com>
2020-05-04 13:47:24 +0300

    layouts

---
+++ app/dz2-2/src/main/java/com/example/dz2_2/MainActivity2_2.java
@@ -0,0 +1,14 @@
+package com.example.dz2_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2_2);
+    }
+}
